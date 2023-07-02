e9d61d0edfdb6c53f134d2d59eae217b715cc0c0
Marilia <marilia.correa.melo@gmail.com>
2023-07-02 19:07:52 -0300

    post participants fixed

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import { MongoClient, ObjectId } from "mongodb"
 import dotenv from "dotenv"
+import dayjs from "dayjs"
 import Joi from "joi"
 
 
